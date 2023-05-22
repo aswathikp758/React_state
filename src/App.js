f7c9c06685826b76e6483f598989e3f611f9bed6
bluba <aswathikp758@gmail.com>
2023-05-22 17:01:10 +0530

    react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    myString:"Hello",
+    myStringOne:123,
+    myStringTwo:true
+  };
+  render(){
+    return(
+      <div className='App'>
+        <h1>Welcome</h1>
+        <h1>{this.state.myString}</h1>
+        <h1>{this.state.myStringOne}</h1>
+        <h1>{this.state.myStringTwo}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
